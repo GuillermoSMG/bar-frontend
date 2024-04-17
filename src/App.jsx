0e70c3e01bf710387c67071e5196a28c30637a8f
Guillermo San Martín <guille.seba0802@gmail.com>
2024-04-17 00:54:17 -0300

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
+import { Navbar } from './components/Navbar';
+import { ProductsContainer } from './components/ProductsContainer';
 
 function App() {
-	return <HomePage />;
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/:category' element={<ProductsContainer />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
